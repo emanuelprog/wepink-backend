35ac2c6f720d27f25f64284974fa5baecea8fc5d
Romeu Bessa <emanuelhamachi@hotmail.com>
2024-10-26 17:22:09 -0400

    Adicionando swagger

---
+++ src/main/java/com/control/wepink/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.control.wepink.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Swagger API")
+                        .version("1.0.0")
+                        .description("Teste")
+                        .contact(new Contact().name("Emanuel Bessa").email("emanuelbessadev@gmail.com")));
+    }
+}
